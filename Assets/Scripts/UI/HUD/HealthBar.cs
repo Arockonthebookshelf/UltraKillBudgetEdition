fbbd1701a4f57de34b62741cede7c7af73dfe691
ManeNaMane <MrMane2005@gmail.com>
2025-02-03 11:46:39 +0530

    .

--- Assets/Scripts/UI/HUD/HealthBar.cs
+++ Assets/Scripts/UI/HUD/HealthBar.cs
@@ -1,18 +1,22 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] Slider healthSlider;
+    [SerializeField] TMP_Text healthText;
 
     public void InitializeHealthBar(int maxHealth)
     {
         healthSlider.value = healthSlider.maxValue = maxHealth;
+        healthText.SetText(healthSlider.value.ToString());
     }
 
     public void UpdateHealthBar(int currentHealth)
     {
         healthSlider.value = currentHealth;
+        healthText.SetText(healthSlider.value.ToString());
     }
 }
 
