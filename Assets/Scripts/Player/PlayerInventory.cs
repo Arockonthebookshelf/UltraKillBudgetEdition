772d8422cfc73785375bb59d36e1db2f58e03265
ManeNaMane <MrMane2005@gmail.com>
2025-02-26 22:25:40 +0530

    .

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] int ClearanceLevel = 0;
+    public int currentBulletCount;
+    public int currentCapacitorCount;
+    public int currentEnergyCellsCount;
+    public int currentRocketsCount;
+
+}
