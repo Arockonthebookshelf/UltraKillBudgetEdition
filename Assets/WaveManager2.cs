41d26827ad62d54bf2349e2c8f395ffe33aa1040
SarthakThakur <sarthakthakur252@gmail.com>
2025-03-16 18:55:07 +0530

    FIX some issue

--- Assets/WaveManager2.cs
+++ Assets/WaveManager2.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class WaveManager2 : MonoBehaviour
 {
-[SerializeField] GameObject firstWave;
+    [SerializeField] GameObject firstWave;
 
+    private Collider triggerCollider;
+
+
+    private void Start()
+    {
+        triggerCollider = GetComponent<Collider>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             firstWave.GetComponent<SingleWave>().SpawnEnemies();
+
+            triggerCollider.enabled = false;
         }
     }
 }
