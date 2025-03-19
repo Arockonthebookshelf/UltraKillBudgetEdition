cb3e8b482a31dadb6071e48bf65505fab0eb5285
ManeNaMane <MrMane2005@gmail.com>
2025-03-19 23:12:27 +0530

    .

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -25,7 +25,6 @@ public class ObjectPooler : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int j = 0;
         foreach (Pool pool in pools)
         {
             string parentName = pool.tag;
