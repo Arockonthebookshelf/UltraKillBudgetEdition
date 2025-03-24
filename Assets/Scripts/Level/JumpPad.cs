ffcc09adf47ece4396f35e32b85f8d44673b16fa
ManeNaMane <MrMane2005@gmail.com>
2025-03-24 22:21:25 +0530

    .

--- Assets/Scripts/Level/JumpPad.cs
+++ Assets/Scripts/Level/JumpPad.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     [SerializeField] float jumpPadStrength = 10f;
+    [SerializeField] float cooldown = 1f;
+    bool canJump = true;
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && canJump)
         {
-            collision.gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * jumpPadStrength , ForceMode.Impulse);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * jumpPadStrength , ForceMode.Impulse);
+            canJump = false;
+            Invoke("Reset", cooldown);
         }
+    }
+    private void Reset()
+    {
+        canJump = true;
     }
 }
