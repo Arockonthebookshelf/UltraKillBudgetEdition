bef3ff847a46a09056054b97dcf5222f0d54d081
Arockonthebookshelf <109503989+Arockonthebookshelf@users.noreply.github.com>
2025-03-25 12:04:54 +0530

    gun fix

--- Assets/Scripts/Backend/SaveSystem/GameData.cs
+++ Assets/Scripts/Backend/SaveSystem/GameData.cs
@@ -18,7 +18,7 @@ public class GameData
    {
       playerPosition = new Vector3(0,0,0);
       ClearanceLevel = 0;
-      currentBulletCount = 0;
+      currentBulletCount = 25;
       currentCapacitorCount = 0;
       currentEnergyCellsCount = 0;
       currentRocketsCount = 0;
