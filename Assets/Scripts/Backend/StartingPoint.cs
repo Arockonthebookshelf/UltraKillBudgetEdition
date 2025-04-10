c34145b8e73d58beedb9c0293a27f71b30839bc0
Arockonthebookshelf <109503989+Arockonthebookshelf@users.noreply.github.com>
2025-04-10 15:15:42 +0530

    changes

--- Assets/Scripts/Backend/StartingPoint.cs
+++ Assets/Scripts/Backend/StartingPoint.cs
@@ -1,12 +1,34 @@
+using System;
 using UnityEngine;
 
 public class StartingPoint : MonoBehaviour
 {
     GameObject player;
+    public bool shotGun;
+    public bool miniGun;
+    public bool rocketLauncher;
+    public PlayerInventory inventory;
+    void OnEnable()
+    {
+        PauseMenu.OnRestart += OnRestart;
+    }
+    void OnDisable()
+    {
+        PauseMenu.OnRestart -= OnRestart;
+    }
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = transform.position;
+        inventory.hasShotgun = shotGun;
+        inventory.hasMinigun = miniGun;
+        inventory.hasRocketLauncher = rocketLauncher;
+    }
+    void OnRestart()
+    {
+        inventory.hasShotgun = shotGun;
+        inventory.hasMinigun = miniGun;
+        inventory.hasRocketLauncher = rocketLauncher;
     }
 
 }
